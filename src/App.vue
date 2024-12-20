ac4d72c385f98923272f2e65b383fec3827fb910
vagner zezo <57641762+vagnerzezo@users.noreply.github.com>
2024-12-19 22:25:36 -0300

    fixed

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
 <template>
-<div>home</div>
+  <Fullbanner />
 </template>
+<script>
+import Fullbanner from './views/fullbanner.vue';
+
+export default {
+  name: "home",
+  components: {
+    Fullbanner
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -3,4 +3,5 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/assets/css/tailwind.css'
+import '@/assets/css/style.css'
 createApp(App).use(store).use(router).mount('#app')
