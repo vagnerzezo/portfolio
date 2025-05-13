19410d320a5364b4ad826915d9ecb29603f37efd
Vagnerzezo <57641762+vagnerzezo@users.noreply.github.com>
2025-05-13 13:41:17 -0300

    fixed links

--- src/components/testimonials/index.vue
+++ src/components/testimonials/index.vue
@@ -6,49 +6,49 @@
           <div
                class="grid grid-cols-1 sm:grid-cols-2 gap-x-3 border-y border-gray-200 divide-y sm:divide-y-0 sm:divide-x divide-gray-200 ">
                <div class="sm:-ms-4 py-6 sm:px-4">
-                         <span class="text-sm text-gray-800 h-32 flex">
-                              Vagner é, sem dúvida, um profissional excepcional e uma pessoa admirável. Possui uma
-                              habilidade única de colaborar em equipe, demonstrando empatia, liderança e comprometimento
-                              em cada projeto que realiza.
-                         </span>
+                    <span class="text-sm text-gray-800 h-32 flex">
+                         Vagner é, sem dúvida, um profissional excepcional e uma pessoa admirável. Possui uma
+                         habilidade única de colaborar em equipe, demonstrando empatia, liderança e comprometimento
+                         em cada projeto que realiza.
+                    </span>
 
-                         <div class="mt-3">
-                              <div class="flex items-center gap-x-2">
-                                   <img class="shrink-0 size-10 rounded-full" src="@/assets/img/feedback/filipe.webp"
-                                        alt="Avatar">
-                                   <div class="grow flex flex-col h-10">
-                                        <div class="text-xs text-gray-500 ">
-                                             Filipe Moura da Silva
-                                        </div>
-                                        <p class="text-xs text-gray-500 ">
-                                             Comercial na SKA
-                                        </p>
+                    <a href="https://www.linkedin.com/in/filipemourasilva/" class="mt-3">
+                         <div class="flex items-center gap-x-2">
+                              <img class="shrink-0 size-10 rounded-full" src="@/assets/img/feedback/filipe.webp"
+                                   alt="Avatar">
+                              <div class="grow flex flex-col h-10">
+                                   <div class="text-xs text-gray-500 ">
+                                        Filipe Moura da Silva
                                    </div>
+                                   <p class="text-xs text-gray-500 ">
+                                        Comercial na SKA
+                                   </p>
                               </div>
                          </div>
+                    </a>
                </div>
 
                <div class="py-6 sm:px-4">
-                   <span class="text-sm text-gray-800 h-32 flex">
-                              O zezo  é um dos profissionais mais dedicados e talentosos que já conheci.
-                              Como empresa, a Agliardi, e eu pessoalmente, somos imensamente gratos por todo o seu
-                              empenho, engajamento e paixão que coloca em tudo que faz.
-                         </span>
+                    <span class="text-sm text-gray-800 h-32 flex">
+                         O zezo é um dos profissionais mais dedicados e talentosos que já conheci.
+                         Como empresa, a Agliardi, e eu pessoalmente, somos imensamente gratos por todo o seu
+                         empenho, engajamento e paixão que coloca em tudo que faz.
+                    </span>
 
-                         <div class="mt-3">
-                              <div class="flex items-center gap-x-2">
-                                   <img class="shrink-0 size-10 rounded-full" src="@/assets/img/feedback/lucas.webp"
-                                        alt="Avatar">
-                                   <div class="grow flex flex-col h-10">
-                                        <div class="text-xs text-gray-500 ">
-                                             Lucas Agliardi
-                                        </div>
-                                        <p class="text-xs text-gray-500 ">
-                                             CEO e Fundador da Agliardi e do Manux Headless
-                                        </p>
+                    <a href="https://www.linkedin.com/in/lucasagliardi/" class="mt-3">
+                         <div class="flex items-center gap-x-2">
+                              <img class="shrink-0 size-10 rounded-full" src="@/assets/img/feedback/lucas.webp"
+                                   alt="Avatar">
+                              <div class="grow flex flex-col h-10">
+                                   <div class="text-xs text-gray-500 ">
+                                        Lucas Agliardi
                                    </div>
+                                   <p class="text-xs text-gray-500 ">
+                                        CEO e Fundador da Agliardi e do Manux Headless
+                                   </p>
                               </div>
                          </div>
+                    </a>
                </div>
           </div>
      </div>
